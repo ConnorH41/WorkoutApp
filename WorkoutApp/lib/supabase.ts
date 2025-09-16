c3600bb9c9aaa496a03d897c9a9e130e4e0c0cce
ConnorHardin <chardin993@g.rwu.edu>
2025-09-16 13:33:02 -0400

    URL + Key

--- WorkoutApp/lib/supabase.ts
+++ WorkoutApp/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://ksoyybbbwtmgegxgrzyz.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtzb3l5YmJid3RtZ2VneGdyenl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgwNDI1NzYsImV4cCI6MjA3MzYxODU3Nn0.iKs_HsK05H2DF0szyvNpjZht7KY3nXGxqIgxsJu6E6o';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
