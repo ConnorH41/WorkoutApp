409fbc728fd8288c2164dea179d5a185c95cb00c
ConnorHardin <chardin993@g.rwu.edu>
2025-10-05 16:08:35 -0400

    Update supabase.ts

--- WorkoutApp/lib/supabase.ts
+++ WorkoutApp/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ksoyybbbwtmgegxgrzyz.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtzb3l5YmJid3RtZ2VneGdyenl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgwNDI1NzYsImV4cCI6MjA3MzYxODU3Nn0.iKs_HsK05H2DF0szyvNpjZht7KY3nXGxqIgxsJu6E6o';
+const supabaseUrl = 'https://lepkzigpfoyvtzltvlmh.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxlcGt6aWdwZm95dnR6bHR2bG1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk2ODk2NTAsImV4cCI6MjA3NTI2NTY1MH0.rXW6pAioyb9YY4ORjyedyVatlXmGTE7nrddva2i3tic';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
