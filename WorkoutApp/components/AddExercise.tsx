3de9c8931cf63da201293dd8343ea5e6b3930d7f
ConnorHardin <chardin993@g.rwu.edu>
2025-10-14 16:40:52 -0400

    DB Update

--- WorkoutApp/components/AddExercise.tsx
+++ WorkoutApp/components/AddExercise.tsx
@@ -141,7 +141,7 @@ export default function AddExercise({ mode = 'modal', onAdd, adding = false, add
                   rightLabel={adding ? 'Adding...' : (addButtonText || 'Add Exercise')}
                   onLeftPress={() => { reset(); setVisible(false); }}
                   onRightPress={handleAdd}
-                  leftColor={colors.textMuted}
+                  leftColor={colors.backgroundMuted}
                   rightColor={colors.primary}
                   leftTextColor={colors.text}
                   rightTextColor={colors.background}
