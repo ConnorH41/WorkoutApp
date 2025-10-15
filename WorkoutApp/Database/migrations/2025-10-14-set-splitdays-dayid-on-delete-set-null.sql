c32beb5db8037839ab07b6f6ad0328e7f0a37b44
ConnorHardin <chardin993@g.rwu.edu>
2025-10-15 14:16:38 -0400

    Deleted Unnecessary Files

--- WorkoutApp/Database/migrations/2025-10-14-set-splitdays-dayid-on-delete-set-null.sql
+++
@@ -1,3 +0,0 @@
--- Migration: Set split_days.day_id foreign key to ON DELETE SET NULL
-ALTER TABLE split_days DROP CONSTRAINT IF EXISTS split_days_day_id_fkey;
-ALTER TABLE split_days ADD CONSTRAINT split_days_day_id_fkey FOREIGN KEY (day_id) REFERENCES days(id) ON DELETE SET NULL;
